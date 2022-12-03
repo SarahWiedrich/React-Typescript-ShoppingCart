11c8ceb4c660710ce5dd74ae14066286ca703f8b
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-12-03 16:46:25 -0600

    text on imageHome Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,12 @@ export function Home(){
     return (
         <>
             <h1>Welcome Home</h1>
-            <div className="bg-image" style={{width: "22rem"}}>
-                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/053.webp" className="w-100" />
-                <div className="mask text-light d-flex justify-content-center flex-column text-center">
+            <div className="bg-image" style={{width: "22rem", position: "relative"}}>
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/053.webp" className="img-thumbnail" />
+                <span className=" text-light card-img-overlay d-flex justify-content-center flex-column text-center" style={{position: "absolute"}}>
                     <h4>Custom heading</h4>
                     <p className="m-0">paragraph</p>
-                </div>
+                </span>
             </div>
         </>
     )    
